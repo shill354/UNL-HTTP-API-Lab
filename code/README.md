4db3e7cadb82380f34bc35862352de9b599f3a9f
Liam Seper <lseper2@huskers.unl.edu>
2022-02-15 13:50:02 -0600

    Initial Commit

---
+++ code/README.md
@@ -0,0 +1,76 @@
+# HTTP & API Basics Lab
+
+## Introduction
+
+The purpose of this lab is to expose you to when HTTP requests are used, and how to use them to interact with Application Programming Interfaces (APIs). It does so via using Javascript's Fetch API, which is a well-used, well-documented API for HTTP Requests in the JavaScript language.
+
+Here are some resources you will probably want a reference to when you complete the lab. Part of the purpose of this lab is to teach you how to research and look into documentation of existing APIs to use them in your future projects and work.
+
+[Javascript's Fetch API Documentation](https://developer.mozilla.org/en-US/docs/Web/API/fetch)
+
+[JavaScript's Await Keyword Documentation](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/await)
+
+[PokeApi's Documentation](https://pokeapi.co/docs/v2)
+
+And again, like all programming tasks, the internet is your friend, and searching for resources that will *help* is encouraged! However, strictly copy and pasting code is *not*, and is not the intention for this lab!
+
+## Getting Started
+
+To begin the lab, first clone the source code by running:
+
+```
+git clone https://github.com/lseper/HTTP-and-API-Lab.git
+```
+
+Then, create a new branch to push to. Do this by running the following command:
+
+```
+git checkout -b Partner1Name-Partner2Name
+```
+
+Where `Partner1Name-Partner2Name` are you and your partner's names. For example, an example branch would look like this:
+
+```
+git checkout -b LiamSeper-GraceClausen
+```
+
+Then, once your branch has been created, run the following command:
+
+```
+git --set-upstream origin Partner1Name-Partner2Name
+```
+The branch name should be the same as the one you created.
+
+Finally, to run the code, simply right click the file `index.html` in your file explorer, and open it in your web browser.
+
+## Your Job
+
+Open the file `index.js`. At the top of the file, there is a function for you to complete, the `getPokemon` function.
+
+The first line of the function fetches the pokemon entered in from the website, and stores it in an array of strings corresponding to each pokemon's name. DO NOT CHANGE THIS LINE. You will be using the variable `pokemon` to complete the rest of the function body.
+
+The function should not return anything. Rather, your task will be to use the PokeApi to retrieve information on each of the pokemon in the `pokemon` array, extract the relevant information, and use the `buildCard` method to display the pokemon on the website.
+
+Note: You do not need to understand how the `buildCard` function works, only what it takes in as arguments, which are defined on the comment above the function.
+
+When you make changes to `index.js`, simply reload the website in your browser for them to take affect on the website.
+
+## Acceptance Criteria
+
+If you successfully complete the function's body, your website should function like so:
+
+1. When valid pokemon are entered into 1, 2, 3, 4, or 5 of the slots, they should appear on the website with the following stats displayed:
+   1. Types
+   2. Abilities
+   3. Base Attack
+   4. Base Defense
+   5. Base Special Attack
+   6. Base Special Defense
+   7. Base HP
+   8. Base Speed
+
+
+
+## Submitting Your Code
+
+Simply push your code to the branch you created when you set up the project. Then you're done!
---
+++ code/index.js
@@ -0,0 +1,118 @@
+// TODO: Create this function
+async function getPokemon() {
+    const pokemon = retrieveInputs();
+}
+
+/**
+ * A function to build the display cards on the website
+ * @param {string} name - the name of the pokemon
+ * @param {array} types - the "types" array that corresponds to the pokemon's types
+ * @param {number} hp - the base HP value of the pokemon
+ * @param {number} attack - the base attack value of the pokemon
+ * @param {number} defense - the base defense value of the pokemon
+ * @param {number} special_attack - the base special attack value of the pokemon
+ * @param {number} special_defense - the base special defense value of the pokemon
+ * @param {number} speed - the base speed value of the pokemon
+ * @param {array} abilities - the "abilities" array that corresponds to the pokemon's abilities
+ * @param {number} height - the height of the pokemon
+ * @param {number} weight - the weight of the pokemon
+ * @param {string} image_url - the url to an in-game sprite used for the pokemon
+ */
+function buildCard(name, types, hp, attack, defense, special_attack, special_defense, speed, abilities, height, weight, image_url) {
+    const card = document.createElement("div");
+    card.className = "card";
+
+    const cardImage = document.createElement("img");
+    cardImage.src = image_url;
+    card.placeholder = `Image of the pokemon: "${name.toUpperCase()}"`;
+    card.appendChild(cardImage);
+
+    const cardName = document.createElement("h1");
+    cardName.innerHTML = name.toUpperCase();
+    card.appendChild(cardName);
+
+    const cardTypeTitle = document.createElement("h2");
+    cardTypeTitle.innerHTML = `<strong>Types</strong>`;
+    card.appendChild(cardTypeTitle);
+    const cardTypesList = document.createElement("ul");
+    for(let i = 0; i < types.length; i++) {
+        const li = document.createElement("li");
+        li.innerHTML = types[i].type.name
+        cardTypesList.appendChild(li);
+    }
+    card.appendChild(cardTypesList);
+
+    const cardAbilitiesListTitle = document.createElement("h2");
+    cardAbilitiesListTitle.innerHTML = "<strong>Abilities<strong>";
+    card.appendChild(cardAbilitiesListTitle);
+    const cardAbilitiesList =  document.createElement("ul");
+    for(let i = 0; i < abilities.length; i++) {
+        const li = document.createElement("li");
+        li.innerHTML = abilities[i].ability.name;
+        cardAbilitiesList.appendChild(li);
+    }
+    card.appendChild(cardAbilitiesList);
+
+    const cardStatisticsTitle = document.createElement("h2");
+    cardStatisticsTitle.innerHTML = "<strong>Stats<strong>";
+    card.appendChild(cardStatisticsTitle);
+
+    const cardStatisticsTable = document.createElement("table");
+    const tr1 = document.createElement("tr");
+    tr1.innerHTML = `<td><strong>Attack: <strong>${attack}</strong></td>
+    <td><strong>Defense: <strong>${defense}</strong></td>`;
+    cardStatisticsTable.appendChild(tr1);
+
+    const tr2 = document.createElement("tr");
+    tr2.innerHTML = `<td><strong>Sp. Atk: <strong>${special_attack}</strong></td>
+    <td><strong>Sp. Def: <strong>${special_defense}</strong></td>`;
+    cardStatisticsTable.appendChild(tr2);
+
+    const tr3 = document.createElement("tr");
+    tr3.innerHTML = `<td><strong>HP: <strong>${hp}</strong></td>
+    <td><strong>Speed: <strong>${speed}</strong></td>`;
+    cardStatisticsTable.appendChild(tr3);
+
+    card.appendChild(cardStatisticsTable);
+
+    const cardMiscStatsContainer = document.createElement("div");
+    const cardHeight = document.createElement("caption");
+    cardHeight.innerHTML = `<strong>Height: </strong>${Math.round((height * 10)) / 100} m`;
+    cardMiscStatsContainer.appendChild(cardHeight);
+    const cardWeight = document.createElement("caption");
+    cardWeight.innerHTML = `<strong>Weight: </strong>${Math.round(weight * 10) / 100} kg`;
+    cardMiscStatsContainer.appendChild(cardWeight);
+
+    card.appendChild(cardMiscStatsContainer);
+
+    const cardContainer = document.getElementById("cardContainer");
+    cardContainer.append(card);
+}
+
+function retrieveInputs() {
+    const inputs = [];
+    for(let i = 1; i < 6; i++) {
+        const currInput = document.getElementById(`pkmn ${i}`);
+        console.log(currInput.value);
+        console.log(currInput.value === "")
+        if(!(currInput.value === "")) {
+            inputs.push(currInput.value);
+            currInput.value = "";
+        }
+    }
+    console.log(inputs);
+    return inputs;
+}
+
+// function to be used in the "get pokemon button". Do not modify this function
+function onClick() {
+    getPokemon();
+}
+
+["pkmn 1", "pkmn 2", "pkmn 3", "pkmn 4", "pkmn 5"].forEach((id) => {
+    document.getElementById(id).addEventListener("keyup", (e) => {
+        if(e.code === "Enter") {
+            document.getElementById("submit").click();
+        }
+    });
+})
